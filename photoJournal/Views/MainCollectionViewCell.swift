0444d31112d7b6ca7bbb62f884b52ee907f9a3cf
howardC56 <h2chang56@gmail.com>
2020-05-02 14:56:41 -0400

    playing around with video options

--- photoJournal/Views/MainCollectionViewCell.swift
+++ photoJournal/Views/MainCollectionViewCell.swift
@@ -27,6 +27,28 @@ final class MainCollectionViewCell: UICollectionViewCell {
         player = nil
     }
     
+    public lazy var activityIndicatorView: UIActivityIndicatorView = {
+        let activity = UIActivityIndicatorView(style: .large)
+        activity.translatesAutoresizingMaskIntoConstraints = false
+        activity.startAnimating()
+        return activity
+    }()
+    
+//    public lazy var pauseButton: UIButton = {
+//        let button = UIButton(type: .system)
+//        button.setImage(UIImage(systemName: "pause"), for: .normal)
+//        button.translatesAutoresizingMaskIntoConstraints = false
+//        button.tintColor = .green
+//        button.addTarget(self, action: #selector(handlePause), for: .touchUpInside)
+//        button.isHidden = true
+//        return button
+//    }()
+//
+//    @objc func handlePause() {
+//        player?.pause()
+//        pauseButton.setImage(UIImage(systemName: "play"), for: .normal)
+//    }
+    
     public lazy var imageView: UIImageView = {
         let image = UIImageView()
         image.clipsToBounds = true
@@ -36,10 +58,24 @@ final class MainCollectionViewCell: UICollectionViewCell {
         return image
     }()
     
+    public lazy var controlsContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0, alpha: 0.5)
+        return view
+    }()
+    
     public lazy var videoView: UIView = {
         let view = UIView()
         view.isHidden = true
-        self.backgroundColor = .clear
+        self.backgroundColor = .black
+//        controlsContainerView.frame = view.frame
+//        view.addSubview(controlsContainerView)
+//        controlsContainerView.addSubview(activityIndicatorView)
+//        activityIndicatorView.centerYAnchor.constraint(equalTo: controlsContainerView.centerYAnchor).isActive = true
+//        activityIndicatorView.centerXAnchor.constraint(equalTo: controlsContainerView.centerXAnchor).isActive = true
+//        controlsContainerView.addSubview(pauseButton)
+//        pauseButton.centerYAnchor.constraint(equalTo: controlsContainerView.centerYAnchor).isActive = true
+//        pauseButton.centerXAnchor.constraint(equalTo: controlsContainerView.centerXAnchor).isActive = true
         return view
     }()
     
@@ -113,12 +149,13 @@ final class MainCollectionViewCell: UICollectionViewCell {
         imageView.image = UIImage(data: currentObject.imageData)
         titleLabel.text = currentObject.title
         descriptionLabel.text = "Date Posted: \(currentObject.date.convertToString()) \n\(currentObject.description)"
-        videoView.isHidden = true
-        imageView.isHidden = false
+//        videoView.isHidden = true
+//        imageView.isHidden = false
         if let videoURL = currentObject.videoData?.convertToURL() {
             videoView.isHidden = false
             imageView.isHidden = true
             player = AVPlayer(url: videoURL)
+            player?.addObserver(self, forKeyPath: "currentItem.loadedTimeRanges", options: .new, context: nil)
             let playerLayer = AVPlayerLayer(player: player)
             playerLayer.frame = videoView.bounds
             playerLayer.videoGravity = .resizeAspect
@@ -126,7 +163,18 @@ final class MainCollectionViewCell: UICollectionViewCell {
             videoView.layer.addSublayer(playerLayer)
             let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(videoPlay))
             videoView.addGestureRecognizer(tap)
+        } else {
+            videoView.isHidden = true
+            imageView.isHidden = false
+        }
     }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "currentItem.loadedTimeRanges" {
+            activityIndicatorView.stopAnimating()
+            controlsContainerView.backgroundColor = .clear
+            //pauseButton.isHidden = false
+        }
     }
     
     @objc func videoPlay() {
@@ -167,5 +215,13 @@ final class MainCollectionViewCell: UICollectionViewCell {
     addSubview(videoView)
        videoView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([videoView.centerXAnchor.constraint(equalTo: centerXAnchor), videoView.topAnchor.constraint(equalTo: topAnchor, constant: 35), videoView.leftAnchor.constraint(equalTo: leftAnchor, constant: 10), videoView.rightAnchor.constraint(equalTo: rightAnchor, constant: -10), videoView.heightAnchor.constraint(equalToConstant: 300)])
+               videoView.addSubview(controlsContainerView)
+               controlsContainerView.frame = videoView.bounds
+               controlsContainerView.addSubview(activityIndicatorView)
+               activityIndicatorView.centerYAnchor.constraint(equalTo: videoView.centerYAnchor).isActive = true
+               activityIndicatorView.centerXAnchor.constraint(equalTo: videoView.centerXAnchor).isActive = true
+//               controlsContainerView.addSubview(pauseButton)
+//               pauseButton.centerYAnchor.constraint(equalTo: videoView.centerYAnchor).isActive = true
+//               pauseButton.centerXAnchor.constraint(equalTo: videoView.centerXAnchor).isActive = true
     }
 }
